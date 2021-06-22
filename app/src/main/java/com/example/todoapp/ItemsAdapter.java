b1a5a1705fba3702a735633e43a3c50c3ff50940
Jessica Lin <jessilin@fb.com>
2021-06-22 15:44:36 -0700

    added items adapter class

---
+++ app/src/main/java/com/example/todoapp/ItemsAdapter.java
@@ -0,0 +1,63 @@
+package com.example.todoapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+/* responsible for taking data from the model (list of strings)
+and displaying it in a row in the recycler view */
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+    /* items in our to-do list */
+    List<String> items;
+
+    public ItemsAdapter(List<String> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ItemsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //use layout inflater to inflate a view
+        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        // wrap it inside a ViewHolder and return it
+        return new ViewHolder(todoView);
+    }
+
+    /* binds data to a particular View Holder */
+    @Override
+    public void onBindViewHolder(@NonNull ItemsAdapter.ViewHolder holder, int position) {
+        // get item at position
+        String item = items.get(position);
+        //bind item into specified view holder
+        holder.bind(item);
+    }
+
+    /* tells recycler view how many items are in the list */
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    /* container to provide easy access to views that represent each row of the list */
+    class ViewHolder extends RecyclerView.ViewHolder {
+        /* text view inside the view that we passed into the constructor */
+        TextView tvItem;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvItem = itemView.findViewById(android.R.id.text1);
+        }
+
+        /* updates the view inside of the View Holder with the item */
+        public void bind(String item) {
+            tvItem.setText(item);
+        }
+    }
+
+}
--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.todoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    /* list of the items in our to-do list */
+    List<String> items;
+
+    /* member variables for UI elements */
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* setting references to UI elements */
+        btnAdd = findViewById(R.id.btnAdd);
+        etItem = findViewById(R.id.etItem);
+        rvItems = findViewById(R.id.rvItems);
+
+        items = new ArrayList<>();
+        /* adding mock data for testing */
+        items.add("Buy milk");
+        items.add("Go to the gym");
+        items.add("Check email");
     }
 }
